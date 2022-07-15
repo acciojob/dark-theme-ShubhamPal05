9266d733e034a32aff908d3c6cdabc92a7f6edf2
unknown <ShubhamPal05>
2022-07-15 11:08:21 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+var button = document.getElementById('swap');
+var BackGround = document.getElementById('app');
 
+console.log(button.classList.length);
+
+button.addEventListener('click', () => {
+    if(button.classList.length == 1){
+        button.classList.add('button_night');
+        BackGround.classList.add('night');
+    }
+    else{
+        button.classList.remove('button_night');
+        BackGround.classList.remove('night');
+    }
+})
